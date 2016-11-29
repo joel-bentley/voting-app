d4cc6745fc30c03ee40d4c4d75ae933656ad5888
Joel Bentley <joel-bentley@users.noreply.github.com>
2016-11-28 21:25:38 -0500

    Add My Polls link to nav bar

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -21,14 +21,22 @@ const NavigationBar = ({ router, isAuthenticated, displayName, avatar, handleLog
               <NavItem href={href} onClick={onClick} eventKey={1}> Home </NavItem>
             )
           }</Link>
+          <Link to="/mypolls">{({ href, onClick }) => (
+              <NavItem href={href} onClick={onClick} eventKey={2}> My Polls </NavItem>
+            )
+          }</Link>
+          <Link to="/mypolls/results">{({ href, onClick }) => (
+              <NavItem href={href} onClick={onClick} eventKey={3}> My Poll Results </NavItem>
+            )
+          }</Link>
         </Nav>
 
         <Nav pullRight>
 
           {isAuthenticated ? (
-              <NavDropdown title={<span><img src={avatar} role="presentation"/>{displayName}</span>} eventKey={3} id="basic-nav-dropdown">
-                <MenuItem onSelect={() => { router.transitionTo('/profile') }} eventKey={3.1}> Profile </MenuItem>
-                <MenuItem onSelect={() => { handleLogout().then( () => { router.transitionTo('/') }) }} eventKey={3.2}> Logout </MenuItem>
+              <NavDropdown title={<span><img src={avatar} role="presentation"/>{displayName}</span>} eventKey={4} id="basic-nav-dropdown">
+                {/* <MenuItem onSelect={() => { router.transitionTo('/profile') }} eventKey={3.1}> Profile </MenuItem> */}
+                <MenuItem onSelect={() => { handleLogout().then( () => { router.transitionTo('/') }) }} eventKey={4.2}> Logout </MenuItem>
               </NavDropdown>
           ) : (
             <Link to="/login">{({ href, onClick }) => (
