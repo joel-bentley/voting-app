0bee46505d6620a381551e4f49587d83daea9c60
Joel Bentley <joel-bentley@users.noreply.github.com>
2016-11-29 15:49:55 -0500

    Add creatorId to poll record

--- server/models/Poll.js
+++ server/models/Poll.js
@@ -3,6 +3,7 @@ var Schema = mongoose.Schema;
 
 var Poll = new Schema({
     pollId: { type: String, default: '' },
+    creatorId: { type: String, default: '' },
     title: { type: String, default: '' },
     choices: { type: Array, default: [] },
     votingRecord: { type: Array, default: [] }
