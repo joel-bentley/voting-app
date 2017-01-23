06a63cde3a6c673b9c277847b8e0be8848b1ee67
Joel Bentley <joel-bentley@users.noreply.github.com>
2017-01-23 15:33:28 -0500

    Fix NavBar collapse on select

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -9,12 +9,32 @@ import {
   NavItem,
 } from 'react-bootstrap';
 
-const NavigationBar = (
-  { router, isAuthenticated, displayName, avatar, handleLogout },
-) =>
-  {
+class NavigationBar extends React.Component {
+  state = { expanded: false };
+
+  toggle = () => {
+    this.setState(prevState => ({ expanded: !prevState.expanded }));
+  };
+
+  close = () => {
+    this.setState({ expanded: false });
+  };
+
+  render() {
+    const {
+      router,
+      isAuthenticated,
+      displayName,
+      avatar,
+      handleLogout,
+    } = this.props;
     return (
-      <Navbar fixedTop>
+      <Navbar
+        fixedTop
+        onToggle={this.toggle}
+        expanded={this.state.expanded}
+        collapseOnSelect
+      >
         <Navbar.Header>
           <Navbar.Brand>
             <Link to="/">
@@ -27,54 +47,53 @@ const NavigationBar = (
         <Navbar.Collapse>
           <Nav>
             <Link to="/">
-              {
-                ({ href, onClick }) => (
-                  <NavItem href={href} onClick={onClick} eventKey={1}>
-                     Home
+              {({ href, onClick }) => (
+                  <NavItem
+                    href={href}
+                    onClick={onClick}
+                    onSelect={this.close}
+                    eventKey={1}
+                  >
+                    Home
                   </NavItem>
-                )
-              }
+                )}
             </Link>
-            {
-              isAuthenticated &&
-                (
+            {isAuthenticated && (
                   <Link to="/mypolls">
-                    {
-                      ({ href, onClick }) => (
-                        <NavItem href={href} onClick={onClick} eventKey={2}>
-                           My Polls
+                    {({ href, onClick }) => (
+                        <NavItem
+                          href={href}
+                          onClick={onClick}
+                          onSelect={this.close}
+                          eventKey={2}
+                        >
+                          My Polls
                         </NavItem>
-                      )
-                    }
+                      )}
                   </Link>
-                )
-            }
-            {
-              isAuthenticated &&
-                (
+                )}
+            {isAuthenticated && (
                   <Link to="/mypolls/results">
-                    {
-                      ({ href, onClick }) => (
-                        <NavItem href={href} onClick={onClick} eventKey={3}>
-                           My Poll Results
+                    {({ href, onClick }) => (
+                        <NavItem
+                          href={href}
+                          onClick={onClick}
+                          onSelect={this.close}
+                          eventKey={3}
+                        >
+                          My Poll Results
                         </NavItem>
-                      )
-                    }
+                      )}
                   </Link>
-                )
-            }
+                )}
           </Nav>
           <Nav pullRight>
-            {
-              isAuthenticated
-                ? <NavDropdown
-                  title={
-                    (
+            {isAuthenticated ? <NavDropdown
+                  title={(
                       <span>
                         <img src={avatar} role="presentation" />{displayName}
                       </span>
-                    )
-                  }
+                    )}
                   eventKey={4}
                   id="basic-nav-dropdown"
                 >
@@ -87,21 +106,23 @@ const NavigationBar = (
                       }}
                     eventKey={4.2}
                   > Logout </MenuItem>
-                </NavDropdown>
-                : <Link to="/login">
-                  {
-                    ({ href, onClick }) => (
-                      <NavItem href={href} onClick={onClick} eventKey={3}>
-                         Login
+                </NavDropdown> : <Link to="/login">
+                  {({ href, onClick }) => (
+                      <NavItem
+                        href={href}
+                        onClick={onClick}
+                        onSelect={this.close}
+                        eventKey={3}
+                      >
+                        Login
                       </NavItem>
-                    )
-                  }
-                </Link>
-            }
+                    )}
+                </Link>}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
     );
-  };
+  }
+}
 
 export default NavigationBar;
